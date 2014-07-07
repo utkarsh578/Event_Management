ea2e77a1d93977a77d554fa7f16544851b401780
chitra8894 <chitra8894@gmail.com>
2014-07-07 19:23:51 +0530

    uses table added

---
+++ laravel/app/database/migrations/2014_07_07_134311_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users',function($table){
+		$table->increments('user_id');
+		$table->string('user_first_name',50);
+        $table->string('user_last_name',50)->nullable();
+		$table->string('user_email',50);
+		$table->string('user_fb_id',50)->nullable();
+		$table->string('user_username',50);
+		$table->string('user_password',70);		
+		$table->string('user_remember_token',70)->nullable();
+		$table->string('user_access_token',70)->nullable();
+		$table->string('user_mobile_no',15);
+		$table->string('user_address',255)->nullable();
+		$table->string('user_profile_pic',255);
+		$table->Timestamps();
+ 		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
