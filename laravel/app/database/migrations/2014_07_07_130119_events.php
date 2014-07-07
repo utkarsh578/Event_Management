31759f1118ec89819f51792f729cde9e0ff40c11
utkarsh578 <utkarsh578@gmail.com>
2014-07-07 23:05:43 +0530

    db is created

--- laravel/app/database/migrations/2014_07_07_130119_events.php
+++ laravel/app/database/migrations/2014_07_07_130119_events.php
@@ -12,7 +12,26 @@ class Events extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('events', function($table){
+			$table->increments("event_id");
+			$table->string("event_title",255);
+			$table->integer("event_location")->foreign('event_location')->references('location_id')->on('location');
+			$table->longText("event_description");
+			$table->string("event_thumbnail",255)->nullable();
+			$table->string("event_banner",255)->nullable();
+			$table->integer("event_rating");
+			$table->string("event_website",255);
+			$table->string("event_fblink",255)->nullable();
+			$table->string("event_twitter",255)->nullable();
+			$table->integer("event_category")->foreign('event_category')->references('category_id')->on('event_category');
+			$table->integer("event_user_created")->foreign('event_user_created')->references('user_id')->on('users');
+			$table->string("event_venue",255);
+			$table->string("event_contact_no",20);
+			$table->string("event_fee",20);
+			$table->integer("event_ticket")->foreign('event_ticket')->references('ticket_id')->on('event_ticket_price');
+			$table->timestamps();
+
+		});
 	}
 
 	/**
@@ -22,7 +41,7 @@ class Events extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('events');
 	}
 
 }
---
+++ laravel/app/database/migrations/2014_07_07_134729_location.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Location extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('location', function($table){
+			$table->increments("location_id");
+			$table->string("location",255);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('location');
+	}
+
+}
